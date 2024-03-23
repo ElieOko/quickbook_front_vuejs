6cedcfc65c8de921b2e8c0ba9d18b78a6a7d53c6
elieoko <elieoko100@gmail.com>
2024-03-23 10:09:35 +0100

    feat :request consum api callback

--- src/views/callback/CallbackView.vue
+++ src/views/callback/CallbackView.vue
@@ -1,4 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { useAxiosRequestWithToken } from '@/utils/service/axios_api';
+import { ApiRoutes } from '@/utils/service/endpoint/api';
+
+
+ (async () => {
+     await (useAxiosRequestWithToken().get(`${ApiRoutes.callback}`)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            console.log("chargement encours...");
+        }));
+})()
+
+</script>
 
 <template>
     <div>
