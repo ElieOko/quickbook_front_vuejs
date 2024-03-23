ca2aed04bc7255067d5c1f944f3011b684c96dc6
elieoko <elieoko100@gmail.com>
2024-03-23 09:45:20 +0100

    feat : view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,67 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="">
+      <router-link
+        class="flex items-center p-4 bg-white border border-1 border-gray-100 rounded-lg shadow-xs dark:bg-gray-800"
+        to="/"
+      >
+        <div
+          class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500"
+        >
+          <i class="w-5 h-5 fa fa-store"></i>
+        </div>
+        <div>
+          <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+            Invoice
+          </p>
+          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+            {{ "1000" }}
+          </p>
+        </div>
+      </router-link>
+    </div>
+    <div class="">
+      <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-2">
+        <div
+          class="bg-no-repeat bg-red-200 border rounded-xl mr-2 p-6"
+          style="
+            background-image: url(https://previews.dropbox.com/p/thumb/AAvyFru8elv-S19NMGkQcztLLpDd6Y6VVVMqKhwISfNEpqV59iR5sJaPD4VTrz8ExV7WU9ryYPIUW8Gk2JmEm03OLBE2zAeQ3i7sjFx80O-7skVlsmlm0qRT0n7z9t07jU_E9KafA9l4rz68MsaZPazbDKBdcvEEEQPPc3TmZDsIhes1U-Z0YsH0uc2RSqEb0b83A1GNRo86e-8TbEoNqyX0gxBG-14Tawn0sZWLo5Iv96X-x10kVauME-Mc9HGS5G4h_26P2oHhiZ3SEgj6jW0KlEnsh2H_yTego0grbhdcN1Yjd_rLpyHUt5XhXHJwoqyJ_ylwvZD9-dRLgi_fM_7j/p.png?fv_content=true&size_mode=5);
+            background-position: 90% center;
+          "
+        >
+          <p class="text-5xl text-indigo-900">
+            Invoice <br /><strong>All data</strong>
+          </p>
+          <span
+            class="bg-red-300 text-xl text-white inline-block rounded-full mt-12 px-8 py-2"
+            ><strong>1000</strong></span
+          >
+        </div>
+
+        <div
+          class="bg-no-repeat bg-orange-200 border rounded-xl ml-2 p-6"
+          style="
+            background-image: url(https://previews.dropbox.com/p/thumb/AAuwpqWfUgs9aC5lRoM_f-yi7OPV4txbpW1makBEj5l21sDbEGYsrC9sb6bwUFXTSsekeka5xb7_IHCdyM4p9XCUaoUjpaTSlKK99S_k4L5PIspjqKkiWoaUYiAeQIdnaUvZJlgAGVUEJoy-1PA9i6Jj0GHQTrF_h9MVEnCyPQ-kg4_p7kZ8Yk0TMTL7XDx4jGJFkz75geOdOklKT3GqY9U9JtxxvRRyo1Un8hOObbWQBS1eYE-MowAI5rNqHCE_e-44yXKY6AKJocLPXz_U4xp87K4mVGehFKC6dgk_i5Ur7gspuD7gRBDvd0sanJ9Ybr_6s2hZhrpad-2WFwWqSNkh/p.png?fv_content=true&size_mode=5);
+            background-position: 100% 40%;
+          "
+        >
+          <p class="text-5xl text-indigo-900">
+            Workers <br /><strong>23</strong>
+          </p>
+          <a
+            href=""
+            class="bg-orange-300 text-xl text-white underline hover:no-underline inline-block rounded-full mt-12 px-8 py-2"
+            ><strong>See messages</strong></a
+          >
+        </div>
+      </div>
+      <div class="flex flex-row h-64 mt-6">
+        <div class="bg-white rounded-xl shadow-lg px-6 py-4 w-4/12">a</div>
+        <div class="bg-white rounded-xl shadow-lg mx-6 px-6 py-4 w-4/12">b</div>
+        <div class="bg-white rounded-xl shadow-lg px-6 py-4 w-4/12">c</div>
+      </div>
+    </div>
   </main>
 </template>
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { OAuthCode } from '@/utils/constante/fun';
+import { useAxiosRequestWithToken } from '@/utils/service/axios_api';
+import { ApiRoutes } from '@/utils/service/endpoint/api';
+
+
+const oauth = async () => {
+    await (useAxiosRequestWithToken().get(`${ApiRoutes.auth}`)
+            .then(function (response) {
+                console.log(response);
+                const url = response.data.url;
+                OAuthCode(url)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                //alert("Elie Oko");
+               
+            }));
+}
+</script>
+<template>
+    <section class="bg-gray-500 min-h-screen flex items-center justify-center">
+  <!-- login container -->
+    <div class="bg-gradient-to-r from-red-500 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+        <!-- form -->
+            <div class="md:w-1/2 px-8 md:px-16">
+            <h2 class="font-bold text-2xl text-[#fff]">Login</h2>
+            <p class="text-xl mt-4 text-[#fff]">Gestion financière des opérations demo</p>
+
+            <form action="" class="flex flex-col gap-4">
+                <input class="p-2 mt-8 rounded-xl border" type="text" name="" placeholder="Nom d'utilisateur">
+                <div class="relative">
+                <input class="p-2 rounded-xl border w-full" type="password" name="password" placeholder="Mot de passe">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                </svg>
+                </div>
+                <button class="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
+            </form>
+
+            <div class="mt-6 grid grid-cols-3 items-center text-gray-400">
+                <hr class="border-gray-400">
+                <p class="text-center text-sm">OR</p>
+                <hr class="border-gray-400">
+            </div>
+
+            <button @click="oauth" class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
+                <img src="https://cdn.worldvectorlogo.com/logos/quickbooks-2.svg" class="mr-2" alt="" width="30" height="30">
+                <span>Connect to QuickBooks</span>
+            </button>
+            </div>
+
+            <!-- image -->
+            <div class="md:block hidden w-1/2">
+            <img class="rounded-2xl" src="https://img.freepik.com/vecteurs-libre/operation-financiere-reussie-comptabilite-entreprise-rapport-facturation-gens-heureux-recu-impot-paiement-droits-economies-argent-revenus-especes-illustration-metaphore-concept-isole-vecteur_335657-4331.jpg">
+            </div>
+        </div>
+    </section>
+</template>
