d714ac1ef1203b63d6237aabc1b13ae7358b3e2e
elieoko <elieoko100@gmail.com>
2024-03-23 09:46:06 +0100

    feat : routing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import InvoiceView from '@/views/invoice/InvoiceView.vue'
+import AppendInvoiceView from '@/views/invoice/AppendInvoiceView.vue'
+import DetailInvoiceView from '@/views/invoice/DetailInvoiceView.vue'
+import WorkerView from '@/views/worker/WorkerView.vue'
+import AppendWorkerView from '@/views/worker/AppendWorkerView.vue'
+import ProfileView from '@/views/company/ProfileView.vue'
+import CallbackView from '@/views/callback/CallbackView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,11 +17,54 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: { layout: 'empty' }
+    },
+    {
+      path: '/invoice',
+      name: 'invoice',
+      component: InvoiceView
+    },
+    {
+      path: '/invoice/new',
+      name: 'invoice_new',
+      component: AppendInvoiceView
+    },
+    {
+      path: '/invoice/detail',
+      name: 'invoice_detail',
+      component: DetailInvoiceView
+    },
+    {
+      path: '/worker',
+      name: 'worker',
+      component: WorkerView
+    },
+    {
+      path: '/worker/new',
+      name: 'worker_new',
+      component: AppendWorkerView,
+    },
+    {
+      path: '/worker/detail',
+      name: 'worker_detail',
+      component: WorkerView
+    },
+    {
+      path: '/company/profile',
+      name: 'company_profile',
+      component: ProfileView
+    },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: CallbackView   
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
