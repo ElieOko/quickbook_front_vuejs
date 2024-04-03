e7edfd0a3023cb94e6265d96cb56187c41197fd9
ElieOko <elieoko100@gmail.com>
2024-04-03 15:05:54 +0100

    fix : bug

--- src/utils/constante/fun.ts
+++ src/utils/constante/fun.ts
@@ -65,6 +65,7 @@ export const OAuthCode: CallbackFunctionAuthCode = async (url: string) :Promise<
               baseURL               : `https://quickbooks.api.intuit.com/v3/company/9341451930708145`,
               headers   : {
                 accept: 'application/json',
+                "Access-Control-Allow-Origin": "*",
                 "Content-type"      :   "application/json",
                 "X-Requested-With"  :   "XMLHttpRequest",
                 "Authorization"     :   `Bearer ${token}`       
